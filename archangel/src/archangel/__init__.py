6776b03724a0366fa1d950cb0aa991161ba01e26
Dev <Dev.Sterling@outlook.com>
2026-03-14 18:03:26 +0530

    some setup

--- archangel/src/archangel/__init__.py
+++ archangel/src/archangel/__init__.py
@@ -1,2 +1,27 @@
-def main() -> None:
-    print("Hello from archangel!")
+import typer
+
+
+app = typer.Typer()
+
+
+@app.callback()
+def callback():
+    """
+    This is ArchAngel
+    """
+
+
+@app.command()
+def locate_conf():
+    """
+    This command scans your system files to find your config files
+    """
+    typer.echo("Locating config files")
+
+
+@app.command()
+def scan_conf():
+    """
+    Scans your config files to see for any vulnerabilities
+    """
+    typer.echo("Checking any vulnerabilities")
